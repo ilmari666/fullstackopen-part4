ff4febe3241f4300e6fbea80bdb8836a999c886d
Ilmari <ilmari@sokeri.com>
2018-06-09 22:31:56 +0300

    split app to modules

--- index.js
+++ index.js
@@ -1,44 +1,20 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs.js');
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+mongoose.connect(process.env.MONGODB_URI);
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/blogs', blogsRouter);
 
-module.exports = Blog
-
-app.use(cors())
-app.use(bodyParser.json())
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
