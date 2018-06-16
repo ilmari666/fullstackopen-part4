0c8eb105ed2f41289243b995b2e099c50aee3658
Ilmari <ilmari@sokeri.com>
2018-06-17 00:04:59 +0300

    refactor controller promise to await, add test to api post

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -20,6 +20,26 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 });
 
+test('new blog is saved', async () => {
+  const newBlog = {
+    title: 'Brave new blog',
+    author: 'Brave new author',
+    url: 'sokeri.com',
+    likes: 0
+  };
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+  const titles = response.body.map(r => r.title);
+
+  expect(response.body.length).toBe(testData.length + 1);
+  expect(titles).toContain('Brave new blog');
+});
+
 afterAll(() => {
   server.close();
 });
